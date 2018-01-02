9496f87ebd899718b498304977f0b4773a9cac64
renyueyue <7606124232qq.com>
2018-01-02 21:06:10 +0800

    tuisong1

--- platform/src/main/webapp/view/system/js/system.js
+++ platform/src/main/webapp/view/system/js/system.js
@@ -1,18 +1,28 @@
 // 推送消息
 function getUnreadMessageCount() {
+    alert("开始");
     $.ajax({
-        url:'../../../../pushmsg',
+        url:'../../../../../pushmsg',
         datatype:'get',
+        beforeSend:function () {
+            alert(111);
+        },
         success:function (response) {
             if(response.code==0){
                 var count=0;
-                if(Utils.isNotNull(response.data)){
+                if(response.data!=null){
+                    console.log(response.data);
                     count = response.data;
                     $('#msgInfo').html("<a href='javascript:void(0);' onclick='showMsgPage();'>"+count+"</a>");
                 }else{
-                    showErrorMsg(response.code, response.message);
+                    console.log(response.code, response.message);
                 }
             }
+        },
+        error:function (response) {
+            console.log('系统错误！');
         }
     });
+    alert('结束');
 }
+getUnreadMessageCount();
